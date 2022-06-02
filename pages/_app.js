8c54e351be78af98531b012b60b0a8c89e34a6f6
unknown <achonwaechris@outlook.com>
2022-06-02 23:27:02 +0100

    adjusting images with chakraui

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,22 @@
 import Router from "next/router"
 import Head from "next/head"
 import Nprogress from 'nprogress'
-import {ChakraProvider} from '@chakra-ui/react'
+import {CharkraProvider} from '@chakra-ui/react'
 import Layout from '../component/Layout'
+ 
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-      <CharkraProvider> 
-        <Layout>
-          <Component {...ChakraProvider}/>
-        </Layout>
-      </CharkraProvider>
-
+     
     </Head>
+    <CharkraProvider> 
+        <Layout>
+          <Component {...pageProps}/>
+        </Layout> 
+      </CharkraProvider>
     </>
   )
 }
